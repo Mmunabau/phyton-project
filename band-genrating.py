81599fa61d136d8edb717bbb85b7482c3e688f1f
Mmunabau <muhammedmunabau@gmail.com>
2024-09-22 09:15:09 +0100

    this is our project initial state

---
+++ band-genrating.py
@@ -0,0 +1,6 @@
+# PROJECT (BAND-NAME-GENERATOR)
+print("WELCOME TO MY NAME GANERATING PROJECT ")
+name_city = input("what city you from?")
+name_pet = input("what the name of your pet?")
+print("My name is alice i leave in " + name_city + " i am twenty four years " + "the name of my pet is "+ name_pet)
+
